d72483700d9a983c89d9bdcaa7ad644800b0d77b
soldier <java_soldiers@163.com>
2019-04-15 01:35:41 +0800

    第三次提交

--- src/main/java/com/ewenkai/demo/controller/DemoController.java
+++ src/main/java/com/ewenkai/demo/controller/DemoController.java
@@ -1,14 +1,29 @@
 package com.ewenkai.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ewenkai.demo.pojo.CstCustomer;
+
+import com.ewenkai.demo.service.CustomerServcie;
 
 
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+	@Autowired
+	private CustomerServcie customerServcie;
+	
 	@RequestMapping("/demo1")
 	public String demo() {
 		return "你好";
+	}
+	@RequestMapping("/findAll")
+	public List<CstCustomer> findAll() {
+		List<CstCustomer> list=customerServcie.findAll();
+		return list;
 	}
 }
